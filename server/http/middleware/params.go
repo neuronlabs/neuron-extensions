c177b6570deec2214c913f6ada58251d06eb5f18
Jacek Kucharczyk <kucjac@gmail.com>
2020-08-17 11:44:32 +0200

    Added http middleware server options.

--- server/http/middleware/params.go
+++ server/http/middleware/params.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 
+	"github.com/neuronlabs/neuron/server"
+
 	"github.com/neuronlabs/neuron-extensions/server/http/httputil"
 	"github.com/neuronlabs/neuron-extensions/server/http/log"
-	"github.com/neuronlabs/neuron/server"
 )
 
 // StoreIDFromParams stores id parameter from the httprouter params under the key: 'idKey'.
@@ -22,6 +24,27 @@ func StoreIDFromParams(idKey string) server.Middleware {
 				return
 			}
 			ctx = httputil.CtxSetID(ctx, params.ByName(idKey))
+			req = req.WithContext(ctx)
+			next.ServeHTTP(rw, req)
+		})
+	}
+}
+
+type serverOptionsKey struct{}
+
+var serverOptionsKeyValue = &serverOptionsKey{}
+
+// CtxGetServerParams gets the server parameters from the context.
+func CtxGetServerOptions(ctx context.Context) (*server.Options, bool) {
+	params, ok := ctx.Value(serverOptionsKeyValue).(*server.Options)
+	return params, ok
+}
+
+// StoreServerOptions stores the server parameters in the request context.
+func StoreServerOptions(options *server.Options) server.Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			ctx := context.WithValue(req.Context(), serverOptionsKeyValue, options)
 			req = req.WithContext(ctx)
 			next.ServeHTTP(rw, req)
 		})
